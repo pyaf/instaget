2cc25b18a077de4e861e2d2d6a2d1db735f61e85
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-08-26 12:12:44 +0530

    Added video download feature

--- instaget/insta/urls.py
+++ instaget/insta/urls.py
@@ -4,5 +4,7 @@ from insta.views import *
 
 urlpatterns = [
     url(r'^$', IndexView),
+    url(r'^video$', VideoView),
+    url(r'^getVideoLink$', getVideoLink),
     url(r'^privacy-policy$', PrivacyPolicy),
 ]
--- instaget/insta/views.py
+++ instaget/insta/views.py
@@ -1,16 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import requests
 # Create your views here.
 def IndexView(request):
     template = 'index.html'
     context = {}
-    if request.method=='POST':
-        post = request.POST
-        post_link = post['post_link']
-        print(post_link)
-        url = 'https://api.instagram.com/oembed/?callback=&url=https://www.instagram.com/p/BYM5yatg17A/'
+    return render(request, template, context)
+
+def VideoView(request):
+    template = 'video.html'
+    context = {}
     return render(request, template, context)
 
 def PrivacyPolicy(request):
     template = 'privacy-policy.html'
     return render(request, template, {})
+
+
+def getPosts(author_name, max_id = ''):
+    url = 'https://www.instagram.com/%s/media/?max_id=%s' % (author_name, max_id)
+    response = requests.get(url).json()
+    return response['items']
+
+
+@csrf_exempt
+def getVideoLink(request):
+    if request.method == "POST":
+        media_id = request.POST['media_id']
+        author_name = request.POST['author_name']
+        foundLink = False
+        max_id = None
+        while(not foundLink):
+            print("Getting Posts..")
+            posts = getPosts(author_name, max_id=max_id)
+            for post in posts:
+                if post['id'] == media_id:
+                    videoLink = post['alt_media_url']
+                    foundLink = True
+            max_id = posts[-1]['id']
+            print(max_id)
+        print('foundLink: ', foundLink)
+        print('videoLink: ', videoLink )
+        return HttpResponse(videoLink)
