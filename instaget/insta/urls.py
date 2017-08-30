60b3ce625c994aa7163e161ae7e88a7f652d2bd0
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2017-08-30 20:43:15 +0530

    multi page done

--- instaget/insta/urls.py
+++ instaget/insta/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url, include
 from insta.views import *
 
 urlpatterns = [
-    url(r'^$', IndexView),
-    url(r'^multi/$', MultiView),
+    url(r'^$', MultiView),
+    # url(r'^multi/$', MultiView),
     url(r'^video/$', VideoView),
     url(r'^account/$', AccountView),
     url(r'^getVideoLink/$', GetVideoLink),
--- instaget/static/js/multi.js
+++ instaget/static/js/multi.js
@@ -19,7 +19,7 @@ function IGembed(shortcode){
             }
             users_posts_dict[data['author_name']][shortcode] = 1;
             var html = `<div class="card">`
-                        + data['html']
+                        + data['html'].replace('data-instgrm-captioned','')
                     + `</div>`;
             $('#results').append(html);
             window.instgrm.Embeds.process() // Note: no semi colon
